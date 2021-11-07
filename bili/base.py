fd5dc7905529725cab96828d47317bb30cfcab69
wyapx <a1025952421@outlook.com>
2021-11-08 00:58:11 +0800

    update: base network implement

--- bili/base.py
+++ bili/base.py
@@ -2,17 +2,25 @@ import aiohttp
 
 
 class Network:
-    def __init__(self) -> None:
-        self._session = aiohttp.ClientSession()
+    def __init__(self, cookies=None) -> None:
+        cookiejar = aiohttp.CookieJar()
+        if cookies:
+            cookiejar.update_cookies(cookies)
+        self._session = aiohttp.ClientSession(cookie_jar=cookiejar)
 
-    def get(self, url: str, data=None) -> dict:
-        pass
+    async def get(self, url: str, params=None) -> dict:
+        async with self._session.get(url, body=params) as resp:
+            if resp.status != 200:
+                raise ConnectionError(resp.status, await resp.read())
+            return await resp.json()
 
-    def post(url: str, data=None) -> dict:
-        pass
-
+    async def post(self, url: str, data=None) -> dict:
+        async with self._session.post(url, data=data) as resp:
+            if resp.status != 200:
+                raise ConnectionError(resp.status, await resp.read())
+            return await resp.json()
 
 
 class APIBase:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, network: Network) -> None:
+        self._network = network
