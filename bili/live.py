debb54d6bd337af2607d609de5eb88d2ded0f615
wyapx <a1025952421@outlook.com>
2021-11-09 08:31:12 +0800

    change: argument

--- bili/live.py
+++ bili/live.py
@@ -1,20 +1,26 @@
-from .base import APIBase
+from .base import APIBase, Network
 
 
 class Live(APIBase):
     base = "https://api.live.bilibili.com"
 
-    async def _get_live(self, room_id: int, call_api: str, need_verify=False) -> dict:
-        return await self.get("/xlive/web-room/v1/index/" + call_api, {"room_id": room_id})
+    def __init__(self, network: Network, room_id: int):
+        super(Live, self).__init__(network)
+        self.room_id = room_id
 
-    async def user_info_in_room(self, room_id: int) -> dict:
-        return await self._get_live(room_id, "getInfoByUser", True)
+    async def _get_live(self, call_api: str, api_version=1) -> dict:
+        return await self.get(f"/xlive/web-room/v{api_version}/index/{call_api}", {"room_id": self.room_id})
 
-    async def room_info(self, room_id: int) -> dict:
-        return await self._get_live(room_id, "getInfoByRoom")
+    async def user_info_in_room(self) -> dict:
+        return await self._get_live("getInfoByUser")
 
-    async def room_play_info(self, room_id: int) -> dict:
-        return await self._get_live(room_id, "getRoomPlayInfo")
+    async def room_info(self) -> dict:
+        return await self._get_live("getInfoByRoom")
+
+    async def room_play_info(self) -> dict:
+        return await self._get_live("getRoomPlayInfo")
 
     async def chat_conf(self, room_id: int) -> dict:
         return await self.get("/room/v1/Danmu/getConf", {"room_id": room_id})
+
+    async def
--- bili/user.py
+++ bili/user.py
@@ -1,20 +1,25 @@
-from .base import APIBase
+from .base import APIBase, Network
 
 
 class User(APIBase):
     base = "https://api.bilibili.com/x"
 
+    def __init__(self, network: Network, uid: int):
+        super(User, self).__init__(network)
+        self.uid = uid
+
     async def my_info(self) -> dict:
         return await self.get("/space/myinfo")
 
-    async def info(self, uid: int) -> dict:
-        return await self.get("/space/acc/info", {"mid": uid})
+    async def info(self) -> dict:
+        return await self.get("/space/acc/info", {"mid": self.uid})
 
-    async def stat(self, uid: int) -> dict:
-        return await self.get("/relation/stat", {"vmid": uid})
+    async def stat(self) -> dict:
+        return await self.get("/relation/stat", {"vmid": self.uid})
 
-    async def upstat(self, uid: int) -> dict:
-        return await self.get("/space/upstat", {"mid": uid})
+    async def upstat(self) -> dict:
+        return await self.get("/space/upstat", {"mid": self.uid})
 
-    async def live_info(self, uid: int) -> dict:
-        return await self.get("/space/acc/info", {"mid": uid})
+    async def live_info(self) -> dict:
+        return await self.get("/space/acc/info", {"mid": self.uid})
+
