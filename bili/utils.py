4f0af4fd34bb519802cfae2bef216efe9a525898
wyapx <a1025952421@outlook.com>
2021-11-11 22:34:43 +0800

    add: LiveDanmuPacket.pack

--- bili/utils.py
+++ bili/utils.py
@@ -1,4 +1,18 @@
+import struct
+
+
 def assert_success(data: dict) -> dict:
     if data.get("code") != 0:
-        raise ConnectionError(data["code"], data["msg"])
+        raise ConnectionError(data["code"], data["message"])
     return data["data"]
+
+
+class LiveDanmuPacket:
+    @staticmethod
+    def pack(data: bytes, proto_ver: int, pkg_type: int) -> bytes:
+        # 12 bytes header + datq
+        return struct.pack("!IHHII", 12, 16, proto_ver, pkg_type, 1) + data
+
+    @staticmethod
+    def unpack(data: bytes):
+        pass
